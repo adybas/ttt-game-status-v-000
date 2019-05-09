2f1ef0154430a5ac23b2246f79e8e8d3d1f08cf4
Anna Dybas <a.m.dybas@gmail.com>
2019-05-09 16:41:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,33 @@
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]] && board[combo[0]] != " "
+  end
+end
+
+=begin
+def full?(board)
+  if position_taken?(board, index).all? {|token| token === "X" || token === "O"}
+    true
+  else
+    false
+  end
+end
+=end
